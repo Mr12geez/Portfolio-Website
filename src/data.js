e0da470bb1ac344533e98df6faf85351e6821748
Chijioke Obiorah <117117741+Mr12geez@users.noreply.github.com>
2023-03-06 15:42:25 -0500

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Todo App",
+    subtitle: "Html, Css, JavaScript",
+    description:
+      "A simple todo app with the ability to add, delete, and save items. Organize tasks by category and set due dates. User-friendly interface for easy task management",
+    image: "./project-4.gif",
+    link: "https://chijiokeobiorah-todo-app.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "He brings his 'A' game. Highly recommend!",
+    image: "https://chidozietech.netlify.app/static/media/profile.2e31d66abcea2429d053.png",
+    name: "Zino Davids",
+    company: "Grandida",
+  },
+  {
+    quote:
+      "i Highly recommend him, he understands the importance of quality digital products for business and possesses necessary skills. it was a Pleasure to work with him",
+    image: "https://media.licdn.com/dms/image/C5603AQEaKvk_AphHCA/profile-displayphoto-shrink_200_200/0/1653327190546?e=1683763200&v=beta&t=tuuSKTS11Havwj-RPbR6-ULGY8SyvcV57yfwszS4Gpg",
+    name: "Mr Marquinhos",
+    company: "Caterpillar Inc",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Html-5",
+  "Node.Js",
+  "Express.Js",
+  "Git",
+];
