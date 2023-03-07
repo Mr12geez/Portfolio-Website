80dd13e9f2152ec0c6dfccfd437222a7d6091c92
Chijioke Obiorah <117117741+Mr12geez@users.noreply.github.com>
2023-03-06 20:55:43 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,12 @@
 
-function App() {
-    return (
-      <div className="App">
-        <header>
-          <h1>My Portfolio</h1>
-        </header>
-        <main>
-        © 2022 CHIJIOKE OBIORAH, ALL RIGHTS RESERVED
-        </main>
-        <Footer />
-      </div>
-    );
-  }
-  
-  export default App;
-  
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>This is my footer.</p>
+    </footer>
+  );
+}
+
+export default Footer;
