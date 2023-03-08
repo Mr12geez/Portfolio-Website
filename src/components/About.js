300e52cf76f4bdb7cd6ac42def49d9a71028b314
Chijioke Obiorah <117117741+Mr12geez@users.noreply.github.com>
2023-03-07 19:55:55 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,12 +17,12 @@ export default function About() {
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Let's Get To Work!
+              Let's Work!
             </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Previous Project
+              Previous Project
             </a>
           </div>
         </div>
