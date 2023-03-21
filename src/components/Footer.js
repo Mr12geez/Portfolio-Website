cd90c68e5d9bc04ec55f2f19ede1b2a982f5e04f
Chijioke Obiorah <117117741+Mr12geez@users.noreply.github.com>
2023-03-21 16:00:43 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,11 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
+    <div className="footer-wrapper">
     <footer>
       <p>Copyright © {currentYear} CHIJIOKE OBIORAH</p>
     </footer>
+    </div>
   );
 }
 
