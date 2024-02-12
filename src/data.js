33c6c6ecff7a43e6164a6252a92d6827aacdf86b
Chijioke Obiorah <117117741+Mr12geez@users.noreply.github.com>
2024-02-12 17:45:45 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Hosting Website",
     description:
       "Hosting website, involves in storing and serving website files on a server, making them accessible to visitors via the internet.",
-    image: "https://images.unsplash.com/photo-1706378886599-41ecb840e43d?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://images.unsplash.com/photo-1664574654578-d5a6a4f447bb?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     link: "https://chijiokeobiorah-hosting-website.netlify.app/",
   },
   {
